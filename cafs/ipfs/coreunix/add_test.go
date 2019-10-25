faf6b4233e59e6c75f998b731e342da2b80f1e50
b5 <sparkle_pony_2000@qri.io>
2019-10-24 20:33:46 -0400

    chore(deps): update go module deps

--- cafs/ipfs/coreunix/add_test.go
+++ cafs/ipfs/coreunix/add_test.go
@@ -151,7 +151,7 @@ func TestAddGCLive(t *testing.T) {
 	defer cancel()
 
 	set := cid.NewSet()
-	err = dag.EnumerateChildren(ctx, dag.GetLinksWithDAG(node.DAG), last, set.Visit)
+	err = dag.Walk(ctx, dag.GetLinksWithDAG(node.DAG), last, set.Visit)
 	if err != nil {
 		t.Fatal(err)
 	}
